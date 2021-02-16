78c3a26ee9e99dce4720ad1e3147747b2550bed9
Hristiyan Stefanov <christian.stefanov@outlook.com>
2021-02-16 20:09:33 +0200

    add ado.net exercises

---
+++ ADO.NET Exercise/MinionNames/Program.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+
+namespace MinionNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connection = new SqlConnection(
+                "Server=.\\SQLEXPRESS;Integrated Security=true;Database=MinionsDB");
+
+            Console.WriteLine("Please enter Villain ID:");
+            var villainId = int.Parse(Console.ReadLine());
+
+            using (connection)
+            {
+                connection.Open();
+
+                var villainNameCmd = new SqlCommand(
+                    $@"SELECT [Name]
+                       FROM Villains
+                       WHERE Id = {villainId}",
+                    connection);
+
+                var minionsCmd = new SqlCommand(
+                    $@"SELECT [Name], Age
+                       FROM Minions m
+                           JOIN MinionsVillains mv ON m.Id = mv.MinionId
+                       WHERE mv.VillainId = {villainId}
+                       ORDER BY [Name]",
+                    connection);
+
+                string villainName = (string)villainNameCmd.ExecuteScalar();
+
+                if (villainName == null)
+                {
+                    Console.WriteLine($"No villain with ID {villainId} exists in the database.");
+                }
+                else
+                {
+                    Console.WriteLine($"Villain: {villainName}");
+
+                    using (var minionsReader = minionsCmd.ExecuteReader())
+                    {
+                        if (minionsReader.HasRows)
+                        {
+                            int rowNumber = 1;
+                            while (minionsReader.Read())
+                            {
+                                Console.WriteLine($"{rowNumber}. {minionsReader["Name"]} {minionsReader["Age"]}");
+                                rowNumber++;
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("(no minions)");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ ADO.NET Exercise/VillainNames/Program.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+
+namespace VillainNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connection = new SqlConnection(
+                "Server=.\\SQLEXPRESS;Integrated Security=true;Database=MinionsDB");
+
+            using (connection)
+            {
+                connection.Open();
+                string cmdString = @"SELECT 
+                                        v.[Name], COUNT(*) AS MinionCount 
+                                     FROM Villains v
+                                        JOIN MinionsVillains mv ON v.Id = mv.VillainId
+                                        JOIN Minions m ON mv.MinionId = m.Id
+                                     GROUP BY v.[Name]
+                                     ORDER BY MinionCount DESC";
+
+                var sqlCmd = new SqlCommand(cmdString, connection);
+
+                using (SqlDataReader sqlReader = sqlCmd.ExecuteReader())
+                {
+                    while (sqlReader.Read())
+                    {
+                        Console.WriteLine($"{sqlReader["Name"]} - {sqlReader["MinionCount"]}");
+                    }
+                }
+            }
+        }
+    }
+}
