8145944edee512c20047b4d7970dc3bcab6ea86f
Hristiyan Stefanov <christian.stefanov@outlook.com>
2021-02-23 18:03:55 +0200

    add

---
+++ ADO.NET Exercise/PrintAllMinionNames/Program.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+
+namespace PrintAllMinionNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connection = new SqlConnection(
+                "Server=.\\SQLEXPRESS;Integrated Security=true;Database=MinionsDB");
+
+            using (connection)
+            {
+                connection.Open();
+    
+                var selectMinionCmd = new SqlCommand(
+                    $@"SELECT 
+		                [Name], ROW_NUMBER() OVER (ORDER BY Id) as RowNum
+	                FROM Minions",
+                    connection);
+
+                var dictionary = new Dictionary<long, string>();
+
+                using (var reader = selectMinionCmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        dictionary.Add((long)reader["RowNum"], (string)reader["Name"]);
+                    }
+
+                    var currentFirstRow = 1;
+                    var currentLastRow = dictionary.Count;
+                    var difference = 0;
+
+                    while (difference < currentLastRow / 2)
+                    {
+                        Console.WriteLine((currentFirstRow + difference) + "  " + dictionary[currentFirstRow + difference]);
+                        Console.WriteLine((currentLastRow - difference) + "  " + dictionary[currentLastRow - difference]);
+                        difference++;
+                    }
+
+                    if (dictionary.Count % 2 != 0)
+                    {
+                        Console.WriteLine((currentLastRow - difference) + "  " + dictionary[currentLastRow - difference]);
+                    }
+                }     
+            }
+        }
+    }
+}
