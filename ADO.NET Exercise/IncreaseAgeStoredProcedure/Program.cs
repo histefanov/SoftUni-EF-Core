f023bb561b620d8d14bcaf032e0fb8f6e6fcc76d
Hristiyan Stefanov <christian.stefanov@outlook.com>
2021-02-23 21:01:12 +0200

    add

---
+++ ADO.NET Exercise/IncreaseAgeStoredProcedure/Program.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+
+namespace IncreaseAgeStoredProcedure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var minionId = int.Parse(Console.ReadLine());
+
+            var connection = new SqlConnection(
+                "Server=.\\SQLEXPRESS;Integrated Security=true;Database=MinionsDB");
+
+            using (connection)
+            {
+                connection.Open();
+
+                var storedProcCmd = new SqlCommand(
+                    $@"EXEC dbo.usp_GetOlder {minionId}",
+                    connection);
+
+                storedProcCmd.ExecuteNonQuery();
+
+                var selectMinionCmd = new SqlCommand(
+                    $@"SELECT [Name], Age
+                       FROM Minions
+                       WHERE Id = {minionId}",
+                    connection);
+
+                using (var reader = selectMinionCmd.ExecuteReader())
+                {
+                    reader.Read();
+                    Console.WriteLine($"{reader["Name"]} - {reader["Age"]} years old");
+                }
+            }
+        }
+    }
+}
---
+++ ADO.NET Exercise/IncreaseMinionAge/Program.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Linq;
+
+namespace IncreaseMinionAge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var minionIds = String.Join(", ", 
+                Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray());
+
+            var connection = new SqlConnection(
+                "Server=.\\SQLEXPRESS;Integrated Security=true;Database=MinionsDB");
+
+            using (connection)
+            {
+                connection.Open();
+
+                var updateCmd = new SqlCommand(
+                    $@"UPDATE Minions
+                    SET Age += 1,
+	                    [Name] = UPPER(LEFT([Name], 1)) +
+			                     LOWER(RIGHT([Name], LEN([Name]) - 1))
+                    WHERE Id IN ({minionIds})",
+                    connection);
+
+                updateCmd.ExecuteNonQuery();
+
+                var selectCmd = new SqlCommand(
+                    @"SELECT [Name], Age
+                      FROM Minions",
+                    connection);
+
+                using (var reader = selectCmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine(reader["Name"] + " " + reader["Age"]);
+                    }
+                }
+            }
+        }
+    }
+}
