700a5a73464bdf015d4cc243a376de1a67a3c73f
Hristiyan Stefanov <christian.stefanov@outlook.com>
2021-02-23 17:25:25 +0200

    add

---
+++ ADO.NET Exercise/RemoveVillain/Program.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+
+namespace RemoveVillain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var villainId = int.Parse(Console.ReadLine());
+            var connection = new SqlConnection(
+                "Server=.\\SQLEXPRESS;Integrated Security=true;Database=MinionsDB");
+
+            using (connection)
+            {
+                connection.Open();
+
+                var villainName = new SqlCommand(
+                    $@"SELECT [Name]
+                       FROM Villains
+                       WHERE Id = {villainId}",
+                    connection)
+                        .ExecuteScalar() as string;
+
+                if (villainName != null)
+                {
+                    var minionCountCmd = new SqlCommand(
+                        $@"SELECT Count(*)
+                           FROM MinionsVillains
+                           WHERE VillainId = {villainId}",
+                        connection);
+
+                    var initialMinionsCount = (int)minionCountCmd.ExecuteScalar();
+
+                    var deleteCmd = new SqlCommand(
+                        $@"BEGIN TRANSACTION
+	                        DELETE FROM MinionsVillains
+	                        WHERE VillainId = {villainId};
+
+	                        DELETE FROM Villains
+	                        WHERE Id = {villainId};
+                        COMMIT",
+                        connection);
+
+                    deleteCmd.ExecuteNonQuery();
+
+                    var finalMinionsCount = (int)minionCountCmd.ExecuteScalar();
+
+                    Console.WriteLine($"{villainName} was deleted.");
+                    Console.WriteLine($"{initialMinionsCount - finalMinionsCount} minions were released.");
+                }
+                else
+                {
+                    Console.WriteLine("No such villain was found.");
+                }
+            }
+        }
+    }
+}
