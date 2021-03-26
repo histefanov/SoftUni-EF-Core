95de5003190daeb1d2ec8b4dc09292d1ee92a31a
Hristiyan Stefanov <christian.stefanov@outlook.com>
2021-03-26 17:43:05 +0200

    add

--- EntityFrameworkIntroduction/SoftUni/StartUp.cs
+++ EntityFrameworkIntroduction/SoftUni/StartUp.cs
@@ -10,7 +10,7 @@ namespace SoftUni
         public static void Main(string[] args)
         {
             var db = new SoftUniContext();
-            var result = GetEmployeesFullInformation(db);
+            var result = GetEmployeesFromResearchAndDevelopment(db);
             Console.WriteLine(result);
         }
 
@@ -35,6 +35,41 @@ namespace SoftUni
             {
                 sb.AppendLine(
                     $"{employee.FirstName} {employee.LastName} {employee.MiddleName} {employee.JobTitle} {employee.Salary:F2}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+        public static string GetEmployeesWithSalaryOver50000(SoftUniContext context)
+        {
+            var employees = context.Employees
+                .Where(x => x.Salary > 50000)
+                .Select(x => new { x.FirstName, x.Salary })
+                .OrderBy(x => x.FirstName);
+
+            var sb = new StringBuilder();
+
+            foreach (var employee in employees)
+            {
+                sb.AppendLine($"{employee.FirstName} - {employee.Salary:F2}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+        public static string GetEmployeesFromResearchAndDevelopment(SoftUniContext context)
+        {
+            var employees = context.Employees
+                .Where(x => x.Department.Name == "Research and Development")
+                .Select(x => new { x.FirstName, x.LastName, DepartmentName = x.Department.Name, x.Salary })
+                .OrderBy(x => x.Salary)
+                .ThenByDescending(x => x.FirstName);
+
+            var sb = new StringBuilder();
+
+            foreach (var employee in employees)
+            {
+                sb.AppendLine($"{employee.FirstName} {employee.LastName} from {employee.DepartmentName} - ${employee.Salary:F2}");
             }
 
             return sb.ToString().TrimEnd();
